2e7f4f1ec3c77beea707631418bec55b76c45858
Leon <eulertech@users.noreply.github.com>
2019-04-22 12:10:50 -0400

    updated driver memory with storage and mem fraction

--- server.R
+++ server.R
@@ -61,7 +61,9 @@ shinyServer(function(input, output) {
     memWOverhead <- memPerExecutor * (1 - input$memOverheadId/100) 
     numParitions <- memPerExecutor * as.integer(input$numExecCoreId) # -- num partitions
     commandString <- sprintf("spark-submit --class <CLASS_NAME> --num-executors %s --executor-cores %s --executor-memory %10.0fMB --driver-memory %10.0fMB --master yarn --deploy-mode client",
-                             numAvailExecutors, input$numExecCoreId, memWOverhead * 1000,memoryPerInstance*.9*1000)
+                             numAvailExecutors, input$numExecCoreId, memWOverhead * 1000,memoryPerInstance*0.6*.9*1000)
+    # driver memory = The default values of spark.storage.memoryFraction and spark.storage.safetyFraction are respectively 0.6 and 0.9 so the real executorMemory
+    #https://umbertogriffo.gitbooks.io/apache-spark-best-practices-and-tuning/content/sparksqlshufflepartitions_draft.html
 #    commandString <- paste("<font color=\"#FF0000\"><b>", commandString, "</b></font>")
     })
   
